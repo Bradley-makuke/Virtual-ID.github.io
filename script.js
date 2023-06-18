ae9c49f2eb4aba6a314626e584d7f6a80c61b260
Paul-NTEE <ntwaetsilepaul@gmail.com>
2023-06-18 22:05:24 +0200

    signup buton

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const wrapper = document.querySelector('.wrapper');
 const btnPopup = document.querySelector('.login-btn');
+const signup_btn = document.querySelector('.signup_btn');
 
 btnPopup.addEventListener('click', function(event){
     wrapper.classList.add('active-popup');
+});
+
+signup_btn.addEventListener('click', function(event){
+    window.open('C:\Users\User\Documents\GitHub\ID-authportal\signup.html');
 });
 
 
