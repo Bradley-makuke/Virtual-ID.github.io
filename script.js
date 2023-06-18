e89f2065b257ae15fa7fd6c2348db4e9b6400a25
Paul-NTEE <ntwaetsilepaul@gmail.com>
2023-06-18 22:23:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const wrapper = document.querySelector('.wrapper');
 const btnPopup = document.querySelector('.login-btn');
-const signup_btn = document.querySelector('.signup-btn');
+const signup = document.querySelector('.signup-btn');
 
 btnPopup.addEventListener('click', function(event){
     wrapper.classList.add('active-popup');
 });
 
-signup_btn.addEventListener('click', function(event){
-    window.location.href('C:\Users\User\Documents\GitHub\ID-authportal\signup.html');
+signup.addEventListener('click', function(event){
+    location.replace('C:\Users\User\Documents\GitHub\ID-authportal\signup.html');
 });
 
 
