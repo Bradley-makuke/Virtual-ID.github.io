c219144f0c660e43e71592b3460e760796adcc00
Paul-NTEE <136990986+Paul-NTEE@users.noreply.github.com>
2023-06-18 19:18:40 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const wrapper = document.querySelector('.wrapper');
+const btnPopup = document.querySelector('.login-btn');
+
+btnPopup.addEventListener('click', function(event){
+    wrapper.classList.add('active-popup');
+});
+
+
+//Gonn Have to add more JS after linking with Brad's Work 
